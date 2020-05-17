03c56735af6dd56efa21360ba40f258e80d76d7a
David <david.bismut@gmail.com>
2020-05-17 18:28:03 +0200

    Make restore work as it should
    Disclaimer: I have very little knowledge of svelte or Apollo.
    
    However, the restore.ts uses the `writeQuery` method to store the query, and the arguments passed to it don't match the API signature.
    
    https://www.apollographql.com/docs/react/v2.4/api/apollo-client/#ApolloClient.writeQuery

--- example/src/Books.svelte
+++ example/src/Books.svelte
@@ -12,7 +12,7 @@
   import { getClient, restore, query } from '../../';
   
   export let cache;
-  restore(client, BOOKS, cache.data);
+  restore(client, { query: BOOKS, data: cache.data });
 
   const books = query(client, { query: BOOKS });
 </script>
