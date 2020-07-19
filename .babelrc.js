b3bc0871e904e3fc1503ece65fff339e2dfb0049
Tim Hall <tim.hall.engr@gmail.com>
2020-07-19 15:12:58 -0400

    chore: update dependencies

--- .babelrc.js
+++
@@ -1,8 +0,0 @@
-const is_test =
-  process.env.NODE_ENV === 'test' || process.env.BABEL_ENV === 'test';
-
-module.exports = {
-  plugins: [is_test && '@babel/plugin-transform-modules-commonjs'].filter(
-    Boolean
-  )
-};
--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,43 @@
-import typescript from 'rollup-plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import filesize from 'rollup-plugin-filesize';
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import filesize from "rollup-plugin-filesize";
 
 const external = [
-  'graphql',
-  'apollo-client',
-  'apollo-utilities',
-  'apollo-link',
-  'svelte',
-  'svelte/store',
-  'svelte-observable',
-  'zen-observable'
+	"graphql",
+	"apollo-client",
+	"apollo-utilities",
+	"apollo-link",
+	"svelte",
+	"svelte/store",
+	"svelte-observable",
+	"zen-observable",
 ];
 
 export default [
-  {
-    input: 'src/index.ts',
-    external,
-    output: [
-      {
-        file: 'dist/svelte-apollo.es.js',
-        format: 'es',
-        sourcemap: true
-      },
-      {
-        file: 'dist/svelte-apollo.cjs.js',
-        format: 'cjs',
-        sourcemap: true
-      }
-    ],
-    plugins: [typescript(), filesize()]
-  },
-  {
-    input: 'src/index.ts',
-    external,
-    output: {
-      file: 'dist/svelte-apollo.d.ts',
-      format: 'es'
-    },
-    plugins: [dts()]
-  }
+	{
+		input: "src/index.ts",
+		external,
+		output: [
+			{
+				file: "dist/svelte-apollo.es.js",
+				format: "es",
+				sourcemap: true,
+			},
+			{
+				file: "dist/svelte-apollo.cjs.js",
+				format: "cjs",
+				sourcemap: true,
+			},
+		],
+		plugins: [typescript(), filesize()],
+	},
+	{
+		input: "src/index.ts",
+		external,
+		output: {
+			file: "dist/svelte-apollo.d.ts",
+			format: "es",
+		},
+		plugins: [dts()],
+	},
 ];
