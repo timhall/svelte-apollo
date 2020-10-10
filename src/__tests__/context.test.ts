e305cc246345823263f511ed387d6e84ee720b26
Tim Hall <tim.hall.engr@gmail.com>
2020-10-10 15:26:37 -0400

    chore: format

--- src/__tests__/context.test.ts
+++ src/__tests__/context.test.ts
@@ -1,28 +1,30 @@
 import { getClient, setClient } from "..";
-import { getContext, setContext } from 'svelte'
+import { getContext, setContext } from "svelte";
 
-jest.mock('svelte')
+jest.mock("svelte");
 
-test('should get client from context', () => {
+test("should get client from context", () => {
 	const client = {};
 
-	(getContext as any).mockReturnValue(client)
-	expect(getClient()).toBe(client)
-})
+	(getContext as any).mockReturnValue(client);
+	expect(getClient()).toBe(client);
+});
 
-test('should throw if client has not been set yet', () => {
-	(getContext as any).mockReturnValue(undefined)
+test("should throw if client has not been set yet", () => {
+	(getContext as any).mockReturnValue(undefined);
 
-	expect(() => getClient()).toThrow(/ApolloClient has not been set yet/)
-})
+	expect(() => getClient()).toThrow(/ApolloClient has not been set yet/);
+});
 
-test('should set client on context', () => {
+test("should set client on context", () => {
 	let client: unknown;
-	
-	(setContext as any).mockImplementation((key: any, value: unknown) => {client = value});
-	(getContext as any).mockImplementation(() => client)
+
+	(setContext as any).mockImplementation((key: any, value: unknown) => {
+		client = value;
+	});
+	(getContext as any).mockImplementation(() => client);
 
 	const value: any = {};
-	setClient(value)
-	expect(getClient()).toBe(value)
-})
+	setClient(value);
+	expect(getClient()).toBe(value);
+});
--- src/__tests__/observable.test.ts
+++ src/__tests__/observable.test.ts
@@ -1,31 +1,36 @@
-import { ApolloError, Observable } from "@apollo/client/core"
-import { GraphQLError } from "graphql"
-import { Data, observableToReadable } from "../observable"
-import { read } from "../__fixtures__/read"
+import { ApolloError, Observable } from "@apollo/client/core";
+import { GraphQLError } from "graphql";
+import { Data, observableToReadable } from "../observable";
+import { read } from "../__fixtures__/read";
 
-test('should return ApolloError for errors', async () => {
+test("should return ApolloError for errors", async () => {
 	const readable = observableToReadable(
-		Observable.of({ errors: [new GraphQLError('GraphQL Error')]})
-	)
+		Observable.of({ errors: [new GraphQLError("GraphQL Error")] })
+	);
 
-	const results = await read(readable)
-	expect(results[0].loading).toBe(true)
-	expect(results[1].error instanceof ApolloError).toBe(true)
-})
+	const results = await read(readable);
+	expect(results[0].loading).toBe(true);
+	expect(results[1].error instanceof ApolloError).toBe(true);
+});
 
-test('should return observable error', async () => {
-	const readable = observableToReadable(new Observable(() => { throw new Error('Internal Error')}))
-
-	const results = await read(readable)
-	expect(results[0].loading).toBe(true)
-	expect(results[1].error?.toString()).toMatch(/Internal Error/)
-})
-
-test('should skip initial loading status, if initial value is given', async () => {
+test("should return observable error", async () => {
 	const readable = observableToReadable(
-		Observable.of({ data: 1 }, { data: 2 }, { data: 3 })
-	, { data: 1} as Data<number>)
+		new Observable(() => {
+			throw new Error("Internal Error");
+		})
+	);
 
-	const values = (await read(readable)).map(result => result.data)
-	expect(values).toEqual([1, 2, 3])
-})
+	const results = await read(readable);
+	expect(results[0].loading).toBe(true);
+	expect(results[1].error?.toString()).toMatch(/Internal Error/);
+});
+
+test("should skip initial loading status, if initial value is given", async () => {
+	const readable = observableToReadable(
+		Observable.of({ data: 1 }, { data: 2 }, { data: 3 }),
+		{ data: 1 } as Data<number>
+	);
+
+	const values = (await read(readable)).map((result) => result.data);
+	expect(values).toEqual([1, 2, 3]);
+});
